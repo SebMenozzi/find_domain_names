74a376238fe46c204afd116e227c1fe8f14d69a1
Sebastien Menozzi <seb.menozzi@hotmail.fr>
2020-07-20 02:44:27 +0200

    minor changes #6

--- find.py
+++ find.py
@@ -5,8 +5,12 @@ import sys
 import os
 import unicodedata
 
-apiKey = 'dLYx8m4oLWgG_QU5RBF48kyWAcgNv2isKGz'
-secretKey = 'KnUE1FzaFFHczBXCoMLaQW'
+def setupQueue(queue):
+    queue.append(('dLYx8m4oLWgG_QU5RBF48kyWAcgNv2isKGz', 'KnUE1FzaFFHczBXCoMLaQW'))
+    queue.append(('dLYx8m4oLWgG_4bpngpmmU7c7YE8nyuE47z', '3NLLBDfUuQQA3UgKZU6QEX'))
+    queue.append(('dLYx8m4oLWgG_Nhw8LL1jugYgr4vTtFJkxB', 'R4cN3FVte4nR43WHNXvCpE'))
+    queue.append(('dLYx8m4oLWgG_5o19oetk8mmpgL9VAk7RSz', 'Nc5cE3HZHM4gVAnP9FkGiM'))
+    queue.append(('dLYx8m4oLWgG_CDLgPbeib1ZuqbuQw2AVbt', 'DoRHknjssdndVkKyJyRqAR'))
 
 def remove_accents(bytes):
     decode = bytes.decode('utf-8')
@@ -32,8 +36,11 @@ def chunks(array, size):
    for i in range(0, len(array), size):
       yield array[i:i + size]
 
-def checkDomains(domains, filteredDomains, i=0):
+def checkDomains(domains, filteredDomains, queue, i=1):
     url = 'https://api.godaddy.com/v1/domains/available'
+
+    (apiKey, secretKey) = queue.pop()
+    queue.insert(0, (apiKey, secretKey))
     headers = { 'Authorization' : 'sso-key {}:{}'.format(apiKey, secretKey) }
 
     availabilityResponse = requests.post(url, json=domains, headers=headers)
@@ -43,38 +50,46 @@ def checkDomains(domains, filteredDomains, i=0):
     try:
         for domain in domains:
             if domain.get('available'):
-                print('\033[0;32;49m{}'.format(domain['domain']))
+                print('\033[0;32;49m{}\033[0;37;49m'.format(domain['domain']))
                 filteredDomains.append(domain['domain'])
             else:
-                print('\033[0;31;49m{}'.format(domain['domain']))
+                print('\033[0;31;49m{}\033[0;37;49m'.format(domain['domain']))
 
-        time.sleep(2)
+        time.sleep(30)
     except:
         if domains is None:
             pass
 
-        print('\033[0;31;49mERROR API')
-        time.sleep(2)
+        print('\033[0;31;49mERROR API\033[0;37;49m')
+        time.sleep(30)
 
-        if i <= 1: # can retry twice
-            checkDomains(domains, filteredDomains, i + 1)
+        if i <= len(queue): # can retry the nb of api key
+            checkDomains(domains, filteredDomains, queue, i + 1)
 
 def filterAvailableDomains(domains):
     chunkSize = 500
     domainChunks = list(chunks(domains, chunkSize))
     filteredDomains = []
+    queue = []
+
+    setupQueue(queue)
 
     for domains in domainChunks:
-        checkDomains(domains, filteredDomains)
+        checkDomains(domains, filteredDomains, queue)
 
     return filteredDomains
 
 def saveDomains(letter, ext, domains):
-    dir = 'results'
+
+    if not os.path.exists('results'):
+        os.mkdir('results')
+
+    dir = 'results/{}'.format(ext)
+
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-    filename = '{}/{}.{}.txt'.format(dir, letter, ext)
+    filename = '{}/{}.txt'.format(dir, letter)
     file = open(filename, 'w')
     file.truncate(0)
 
